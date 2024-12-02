97956bf056e29786abd4ed89493479eb8ce8f59d
Jamesb <experimentallearning0@gmail.com>
2024-12-02 08:04:06 +0000

    try fix

--- extra/utilities/blake3.py
+++ extra/utilities/blake3.py
@@ -1,5 +1,5 @@
 import math
-from typing import Dict, List, Optional, Tuple
+from typing import Callable, List, Optional, Tuple
 from tinygrad.dtype import dtypes
 from tinygrad.engine import jit
 from tinygrad.helpers import ceildiv
@@ -9,6 +9,7 @@ class BLAKE3:
   def __init__(self, std_sizes: Optional[List[int]] = None):
     self.IV = Tensor([0x6A09E667, 0xBB67AE85, 0x3C6EF372, 0xA54FF53A, 0x510E527F, 0x9B05688C, 0x1F83D9AB, 0x5BE0CD19], dtype=dtypes.uint32)
     self.std_sizes = std_sizes or [1024**3]
+    self.PAD, self.DEFAULT_LEN, self.PERM = 66, 65, Tensor([2, 6, 3, 10, 7, 0, 4, 13, 1, 11, 12, 5, 9, 14, 15, 8], dtype=dtypes.uint32)
 
   @jit.TinyJit
   def mix(self, states: Tensor, chunks: Tensor) -> Tensor:
@@ -21,85 +22,101 @@ class BLAKE3:
         states[c] = states[c] + states[d]
         states[b] = rotr(states[b] ^ states[c], 12 if m is mx else 7)
 
-  def compress_chunks(self, states: Tensor, chunks: Tensor, chain_vals: Tensor, len_info: Dict) -> Tensor:
-    n_end_blocks, n_chunks = len_info["n_end_blocks"], len_info["n_chunks"]
+  def compress_chunks(self, states: Tensor, data: Tensor, chain_vals: Tensor, info: Tensor) -> Tensor:
     for i in range(16): # parallel over chunks, sequential over blocks
-      compressed = self.compress_blocks(states[i].contiguous().realize(), chunks[i].contiguous().realize(), chain_vals[i].contiguous().realize())
-      if i < chunks.shape[1] - 1: states[i + 1, :8] = compressed[:8] # propagate chain vals
-      if i == n_end_blocks - 1: final_chain_val = compressed[:8, n_chunks - 1].reshape(-1, 1) # for partial chunks
-    return compressed[:8, :n_chunks] if n_end_blocks == 16 else compressed[:8, :n_chunks - 1].cat(final_chain_val, dim=-1)
+      self.compress_blocks(states[i].contiguous(), data[i].contiguous(), chain_vals[i].contiguous())
+      if i < data.shape[1] - 1: states[i + 1, :8] = states[i, :8] # propagate chain vals
+    states = states * (info < self.PAD) # zero out padding
+    end_block = (states * (info < self.DEFAULT_LEN)).sum(0) # pick out the end block
+    return (states[-1, :] | end_block)[:8] # combine last block of each chunk with end block
 
-  def compress_blocks(self, states: Tensor, chunks: Tensor, chain_vals: Tensor) -> Tensor:
+  def compress_blocks(self, states: Tensor, data: Tensor, chain_vals: Tensor) -> Tensor:
     for _ in range(6):
-      self.mix(states, chunks)
-      chunks[:] = chunks[[2, 6, 3, 10, 7, 0, 4, 13, 1, 11, 12, 5, 9, 14, 15, 8]]
-    self.mix(states, chunks)
+      self.mix(states, data)
+      data = data[self.PERM]
+    self.mix(states, data)
     states[:8] = states[:8] ^ states[8:]
     states[8:] = chain_vals[:8] ^ states[8:]
     return states
 
-  def tensor_to_blake_data(self, tensor: Tensor) -> Tuple[Tensor, int, int]:
+  def tensor_to_blake_data(self, tensor: Tensor) -> Tuple[Tensor, Tensor]:
     size = min(size for size in self.std_sizes if size >= tensor.nbytes()) // tensor.element_size()
-    data = tensor.pad(((0, size - tensor.shape[0],),), value=0) # TODO: flatten()
+    data = tensor.flatten().pad(((0, size - tensor.shape[0],),), value=0)
     data = data.bitcast(dtypes.uint32).reshape(-1, 16, 16).permute(1, 2, 0).contiguous()
     final_chunk_len = 0 if tensor.nbytes() == 0 else (tensor.nbytes() % 1024 or 1024)
     n_end_blocks = ceildiv(final_chunk_len, 64) or 1
-    end_block_len = 0 if tensor.nbytes() == 0 else tensor.nbytes() % 64 or 64
     n_chunks = max(1, ceildiv(tensor.nbytes(), 1024))
-    return data, {"n_end_blocks": n_end_blocks, "end_block_len": end_block_len, "n_chunks": n_chunks}
+    info = Tensor.full((16, 1, data.shape[-1]), fill_value=self.DEFAULT_LEN, dtype=dtypes.uint32).contiguous()
+    info[n_end_blocks - 1, :, n_chunks - 1] = 0 if tensor.nbytes() == 0 else (tensor.nbytes() % 64) or 64
+    info[n_end_blocks:, :, n_chunks - 1:] = info[:, :, n_chunks:] = self.PAD
+    n_steps = math.ceil(math.log2(max(n_chunks, 1)))
+    return data.contiguous(), info.contiguous(), n_steps
 
-  def pairwise_concat(self, chain_vals: Tensor) -> Tuple[Tensor, Optional[Tensor]]:
-    leftover_chunk = chain_vals[:, -1:] if chain_vals.shape[1] % 2 else None
-    chain_vals = chain_vals[:, :-1 if leftover_chunk is not None else None]
-    return chain_vals.permute(1, 0).reshape(-1, 16).transpose().contiguous(), leftover_chunk
+  def pairwise_concat(self, chain_vals: Tensor) -> Tuple[Tensor, Tensor]:
+    paired_cvs_with_leftover = chain_vals.permute(1, 0).reshape(-1, 16).transpose()
+    paired = chain_vals.any(0).reshape(-1, 2)
+    paired_mask = (paired[:, 0] * paired[:, 1])
+    leftover_mask = (paired[:, 0] ^ paired[:, 1])
+    paired_cvs = (paired_cvs_with_leftover * paired_mask).pad(((0, 0), (0, paired.shape[0])), value=0)
+    leftover = (paired_cvs_with_leftover * leftover_mask).sum(1)[:8].reshape(-1, 1)
+    return paired_cvs, leftover
 
-  def create_state(self, iv: Tensor, count: Optional[int], len_info: Dict, flags: Tensor) -> Tensor:
-    n_blocks, _, total_chunks = iv.shape
-    n_end_blocks, end_block_len, n_chunks = len_info["n_end_blocks"], len_info["end_block_len"], len_info["n_chunks"]
-    if count is not None:
-      counts = Tensor.arange(count, count + total_chunks, dtype=dtypes.uint32).reshape(-1, 1).expand(-1, 16).reshape(-1, 16, 1)
-      counts = counts.permute(1, 2, 0).cat(Tensor.zeros(n_blocks, 1, total_chunks, dtype=dtypes.uint32), dim=1)
-    else:
-      counts = Tensor.zeros((n_blocks, 2, total_chunks), dtype=dtypes.uint32)
-    lengths = Tensor.full((n_blocks, 1, total_chunks), fill_value=64, dtype=dtypes.uint32).contiguous()
-    if end_block_len is not None: lengths[n_end_blocks - 1, :, n_chunks - 1] = end_block_len
-    return iv.cat(iv[:, :4], counts, lengths, flags, dim=1)
+  def create_state(self, iv: Tensor, counts: Tensor, info: Tensor, flags: Tensor) -> Tensor:
+    counts = counts.cat(Tensor.zeros(iv.shape[0], 1, iv.shape[-1], dtype=dtypes.uint32), dim=1)
+    lengths = (info == self.DEFAULT_LEN).where(64, info) # set default lengths
+    states = iv.cat(iv[:, :4], counts, lengths, flags, dim=1) # create states
+    return states * (info < self.PAD).cast(dtypes.uint32) # zero out padding
 
-  def create_flags(self, data: Tensor, len_info: Dict, root: bool, parent: bool, final_step: bool) -> Tensor:
-    block_end_idx = len_info["n_end_blocks"] - 1 if len_info["n_end_blocks"] is not None else -1
-    chunk_end_idx = len_info["n_chunks"] - 1
-    flags = Tensor.zeros((16, 1, data.shape[-1]), dtype=dtypes.uint32).contiguous()
+  def create_flags(self, info: Tensor, parents: Tensor, final_step: Tensor) -> Tensor:
+    flags = Tensor.zeros((16, 1, info.shape[-1]), dtype=dtypes.uint32).contiguous()
+    flags[-1, 0] = flags[-1, 0] + 2 # chunk end flag
+    flags = (flags + 2 * ((flags != 2) * (info < self.DEFAULT_LEN))) # chunk end flag for partial final chunk
     flags[0] = flags[0] + 1 # chunk start flag
-    flags[-1, 0, :chunk_end_idx] = flags[-1, 0, :chunk_end_idx] + 2 # chunk end flag
-    flags[block_end_idx:, :, chunk_end_idx] = flags[block_end_idx:, :, chunk_end_idx] + 2 # final chunk end flag for partial chunk
-    if parent: flags[:] = 4 # parent flag
-    if root: flags[block_end_idx, :, chunk_end_idx] = flags[block_end_idx, :, chunk_end_idx] + 8 # root flag
-    if final_step: flags[:] = 12
-    return flags
+    flags = parents.where(4, flags) # parent flag
+    flags = (flags + (8 * (((info < self.PAD).sum() <= 16) * (info < self.DEFAULT_LEN)))) # root flag if <= 1 chunk
+    flags = final_step.where(12, flags) # final step flag
+    flags = flags * (info < self.PAD)
+    return flags.cast(dtypes.uint32)
 
-  def compress(self, data, compressor, count, len_info, root, parent = False, final_step = False) -> Tensor:
-    iv = self.IV.reshape(1, 8, 1).expand(16, 8, data.shape[-1]).contiguous()
-    states = self.create_state(iv, count, len_info, self.create_flags(data, len_info, root, parent, final_step))
-    return compressor(states, data, iv, len_info)
+  def compress(self, data: Tensor, compressor: Callable, counts: Tensor, info: Tensor, parents: Tensor, final_step: Tensor) -> Tensor:
+    iv = self.IV.reshape(1, 8, 1).expand(16, 8, info.shape[-1]).contiguous()
+    flags = self.create_flags(info, parents, final_step)
+    states = self.create_state(iv, counts, info, flags)
+    return compressor(states, data, iv, info)
 
   def compress_tree(self, states, data, iv, _): return self.compress_blocks(states[-1].contiguous(), data, iv[0])
 
-  def hash(self, tensor: Tensor) -> str:
-    data, len_info = self.tensor_to_blake_data(tensor)
-    chain_vals = self.compress(data, self.compress_chunks, 0, len_info, len_info["n_chunks"] == 1)
-    n_steps = math.ceil(math.log2(max(chain_vals.shape[-1], 1)))
-    len_info["end_block_len"] = None # don't need end block length for tree hashing
-    for i in range(n_steps): # tree-hash chain value pairs ~halving them in each step
+  def _hash(self, data: Tensor, info: Tensor) -> Tensor:
+    parents = Tensor.zeros((16, 1, data.shape[-1]), dtype=dtypes.bool).contiguous()
+    final_step = Tensor.zeros((16, 1, data.shape[-1]), dtype=dtypes.bool).contiguous()
+    counts = Tensor.arange(0, data.shape[-1], dtype=dtypes.uint32).reshape(-1, 1).expand(-1, 16).reshape(-1, 16, 1).permute(1, 2, 0)
+    chain_vals = self.compress(data, self.compress_chunks, counts, info, parents, final_step)
+    info = (info < self.DEFAULT_LEN).where(64, info)
+    counts = Tensor.zeros((16, 1, data.shape[-1]), dtype=dtypes.uint32)
+    parents = Tensor.ones((16, 1, data.shape[-1]), dtype=dtypes.bool)
+    final_step = chain_vals.any(0).sum(-1) == 2
+    tree_steps = math.ceil(math.log2(max(data.shape[-1], 1)))
+    results = Tensor.zeros((tree_steps + 1, 8), dtype=dtypes.uint32).contiguous()
+    results[0] = chain_vals[:, 0]
+    for i in range(tree_steps): # tree-hash chain value pairs ~halving them in each step
       chain_vals, leftover_chain_val = self.pairwise_concat(chain_vals)
-      pre_pad_size = chain_vals.shape[1]
-      pad_size = data.shape[2] - pre_pad_size # padding for JIT consistency
-      chain_vals = chain_vals.pad(((0, 0), (0, pad_size)), value=0) if i < n_steps - 1 else chain_vals.expand(-1, pre_pad_size + pad_size)
-      chain_vals = self.compress(chain_vals.contiguous(), self.compress_tree, None, len_info, i == n_steps - 1, True, i == n_steps - 1)
-      chain_vals = chain_vals[:8, :pre_pad_size]
-      if leftover_chain_val is not None: chain_vals = chain_vals.cat(leftover_chain_val, dim=1)
-    return chain_vals.flatten().bitcast(dtypes.uint8).data().tobytes().hex()
+      valid = chain_vals.any(0)
+      chain_vals = self.compress(chain_vals.contiguous(), self.compress_tree, counts, info, parents, final_step)[:8] * valid
+      results[i + 1] = chain_vals[:, 0]
+      insertion_mask = (valid ^ valid.roll(1, -1))
+      insertion_mask[0] = 0
+      chain_vals = insertion_mask.where(leftover_chain_val, chain_vals)
+      final_step = chain_vals.any(0).sum(-1) == 2
+    return results.realize()
+
+  def hash(self, tensor: Tensor) -> str:
+    data, info, n_steps = self.tensor_to_blake_data(tensor)
+    results = self._hash(data, info)
+    return results[n_steps].flatten().bitcast(dtypes.uint8).data().tobytes().hex()
 
 if __name__ == "__main__":
+  # t = Tensor.full((1024 ** 2) * 500, fill_value=1, dtype=dtypes.uint8)
+  # print(BLAKE3().hash(t))
   import time
   import sys
   import random
