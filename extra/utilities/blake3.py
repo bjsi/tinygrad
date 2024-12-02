734e907e1a9e83eeb16eac1d72a5b31000b2fa84
Jamesb <experimentallearning0@gmail.com>
2024-12-02 16:02:35 +0000

    try fix

--- extra/utilities/blake3.py
+++ extra/utilities/blake3.py
@@ -92,7 +92,7 @@ class BLAKE3:
 
   def compress_tree(self, states, data, iv, _): return self.compress_blocks(states[-1].contiguous(), data, iv[0]).clone().realize()
 
-  def _hash(self, data: Tensor, info: Tensor) -> Tensor:
+  def _hash(self, data: Tensor, info: Tensor, n_steps: int) -> Tensor:
     parents = Tensor.zeros((16, 1, data.shape[-1]), dtype=dtypes.bool).contiguous()
     final_step = Tensor.zeros((16, 1, data.shape[-1]), dtype=dtypes.bool).contiguous()
     counts = Tensor.arange(0, data.shape[-1], dtype=dtypes.uint32).reshape(-1, 1).expand(-1, 16).reshape(-1, 16, 1).permute(1, 2, 0)
@@ -104,7 +104,7 @@ class BLAKE3:
     tree_steps = math.ceil(math.log2(max(data.shape[-1], 1)))
     results = Tensor.zeros((tree_steps + 1, 8), dtype=dtypes.uint32).contiguous()
     results[0] = chain_vals[:, 0]
-    for i in range(tree_steps): # tree-hash chain value pairs ~halving them in each step
+    for i in range(n_steps): # tree-hash chain value pairs ~halving them in each step
       chain_vals, leftover_chain_val = self.pairwise_concat(chain_vals)
       valid = chain_vals.any(0)
       chain_vals = self.compress(chain_vals.contiguous(), self.compress_tree, counts, info, parents, final_step)[:8] * valid
@@ -117,7 +117,7 @@ class BLAKE3:
 
   def hash(self, tensor: Tensor) -> str:
     data, info, n_steps = self.tensor_to_blake_data(tensor)
-    results = self._hash(data, info)
+    results = self._hash(data, info, n_steps)
     return results[n_steps].flatten().bitcast(dtypes.uint8).data().tobytes().hex()
 
 if __name__ == "__main__":
