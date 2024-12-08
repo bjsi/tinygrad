dc67585a4ebb995c12a93f62e13eaa1db1d2ecb4
Jamesb <experimentallearning0@gmail.com>
2024-12-08 11:30:35 +0000

    test

--- extra/utilities/blake3.py
+++ extra/utilities/blake3.py
@@ -23,7 +23,6 @@ class BLAKE3:
       if i < 6: data = data[self.PERMUTATIONS]
     return (states[:8] ^ states[8:]).cat(chain_vals[:8] ^ states[8:]).realize()
 
-  @TinyJit
   def init_chain_vals(self, data: Tensor, info: Tensor) -> Tuple[Tensor, Tensor]:
     chain_vals = self.IV.reshape(1, 8, 1).expand(16, 8, info.shape[-1]).contiguous()
     counts = Tensor.arange(0, data.shape[-1], dtype=dtypes.uint32).reshape(-1, 1).expand(-1, 16).reshape(-1, 16, 1).permute(1, 2, 0)
@@ -42,7 +41,6 @@ class BLAKE3:
     end_block = (states * (info < self.DEFAULT_LEN)).sum(0)
     return (states[-1, :] | end_block)[:8].realize()
 
-  @TinyJit
   def tree_hash(self, chain_vals: Tensor, n_tree_steps: Variable) -> Tensor:
     for _ in range(n_tree_steps.val):
       stacked = chain_vals.transpose().reshape(-1, 16).transpose().reshape(2, 8, -1)
@@ -70,11 +68,16 @@ class BLAKE3:
     info[n_end_blocks:, :, n_chunks - 1:] = info[:, :, n_chunks:] = self.PAD
     n_steps = Variable(min_val=0, max_val=math.log2(max_memory), name="n_steps").bind(math.ceil(math.log2(max(n_chunks, 1))))
     return data, info, n_steps
+  
+  @TinyJit
+  def _hash(self, data: Tensor, info: Tensor, n_tree_steps: Variable) -> Tensor:
+    chain_vals = self.init_chain_vals(data, info)
+    chain_vals = self.tree_hash(chain_vals, n_tree_steps)
+    return chain_vals
 
   def hash(self, tensor: Tensor, max_memory: int = 1024**3) -> str:
     data, info, n_tree_steps = self.tensor_to_blake_input(tensor, max_memory)
-    chain_vals = self.init_chain_vals(data, info)
-    chain_vals = self.tree_hash(chain_vals, n_tree_steps)
+    chain_vals = self._hash(data, info, n_tree_steps)
     return chain_vals[:, 0].flatten().bitcast(dtypes.uint8).data().tobytes().hex()
 
 if __name__ == "__main__":
